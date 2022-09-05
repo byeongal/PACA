737b28f8fa557c02c52162a8d0d10ad7e39ca7cd
YoungJae Kim <corea_kyj@naver.com>
2022-09-05 18:04:51 +0900

    base code

---
+++ src/app.py
@@ -0,0 +1,108 @@
+import os
+import uuid
+from datetime import datetime
+
+import firebase_admin
+import openai
+import streamlit as st
+from firebase_admin import credentials, db
+from streamlit_chat import message as st_message
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+try:
+    firebase_cred_path = os.getenv("FIREBASE_CRED_PATH")
+    firebase_database_url = os.getenv("FIREBASE_DATABASE_URL")
+    cred = credentials.Certificate(firebase_cred_path)
+    firebase_admin.initialize_app(
+        cred,
+        {"databaseURL": firebase_database_url},
+    )
+    print("Done")
+except Exception as e:
+    print(f"Error: {e}")
+
+
+@st.experimental_singleton
+def load_initial_data():
+    print("Load Data")
+    task_description = "The following is a conversation with an AI assistant."
+    brooklyn_data = db.reference("/apps/brooklyn").get()
+    print(brooklyn_data)
+    core_beliefs = " ".join(
+        [value for _, value in sorted(brooklyn_data["core_beliefs"].items(), key=lambda x: int(x[0]))]
+    )
+    base_context = [
+        {"AI": value["AI"], "Human": value["Human"], "context_id": key}
+        for key, value in sorted(brooklyn_data["context"].items(), key=lambda x: int(x[0]))
+    ]
+
+    return task_description, core_beliefs, base_context
+
+
+st.title("PACA")
+
+
+def generate_answer():
+    now = int(datetime.utcnow().timestamp() * 1000)
+    task_description, core_beliefs, base_context = load_initial_data()
+    if "prompt_history" not in st.session_state:
+        st.session_state.prompt_history = base_context
+    user_message = st.session_state.input_text
+    if user_message.startswith("No way! You should say"):
+        fixed_bot_start_idx = len("No way! You should say")
+        fixed_bot_response = user_message[fixed_bot_start_idx:].strip()
+        prev = st.session_state.prompt_history.pop()
+        st.session_state.prompt_history.append(
+            {"Human": prev["Human"], "AI": fixed_bot_response, "context_id": str(now)}
+        )
+        # History 에서 잘못된 말 삭제 + 새로운 말 추가
+        db.reference("/apps/brooklyn/history").update(
+            {
+                prev["context_id"]: None,
+                str(now): {"Human": prev["Human"], "AI": fixed_bot_response},
+            }
+        )
+        # Context 에 새로운 데이터 추가
+        db.reference("/apps/brooklyn/context").update(
+            {
+                str(now): {"Human": prev["Human"], "AI": fixed_bot_response, "is_fixed": True},
+            }
+        )
+        message_bot = f"Right, {fixed_bot_response}"
+    else:
+        context = "\n".join([f"Human: {each['Human']}\nAI: {each['AI']}" for each in st.session_state.prompt_history])
+        context += f"\nHuman: {user_message}\nAI:"
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=f"{task_description} {core_beliefs}\n\n{context}",
+            temperature=0.9,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.6,
+            stop=[" Human:", " AI:"],
+        )
+        message_bot = response["choices"][0]["text"].strip()
+        st.session_state.prompt_history.append({"Human": user_message, "AI": message_bot, "context_id": str(now)})
+        # History 새로운 말 추가
+        db.reference("/apps/brooklyn/history").update(
+            {
+                str(now): {"Human": user_message, "AI": message_bot},
+            }
+        )
+    user_id = str(uuid.uuid5(uuid.NAMESPACE_OID, str(now) + "user"))
+    bot_id = str(uuid.uuid5(uuid.NAMESPACE_OID, str(now) + "bot"))
+    st.session_state.history.append({"message": user_message, "is_user": True, "key": user_id})
+    st.session_state.history.append({"message": message_bot, "is_user": False, "key": bot_id})
+
+
+st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
+
+for chat in st.session_state.history:
+    st_message(**chat)  # unpacking
